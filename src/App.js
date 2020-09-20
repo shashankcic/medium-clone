22083599bc635a26d6dd9759cc422dc46d57d49f
Shashank Singh <singhshashankcic@ducic.ac.in>
2020-09-20 19:07:48 +0000

    Adding logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src='logo192.png' alt='Medium Logo' />
         <h1>Medium Clone App in React</h1>
       </header>
     </div>
